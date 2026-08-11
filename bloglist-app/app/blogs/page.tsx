474d635d66dcf20b23b2485456764d208f673d52
Otto Karppinen <ottokarpp@gmail.com>
2026-08-11 17:41:09 +0300

    exerc_5 blogs sorted by likes

--- bloglist-app/app/blogs/page.tsx
+++ bloglist-app/app/blogs/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { blogs } from "../lib/blogs";
 
 export default function BlogsPage() {
+  const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes);
+
   return (
     <div>
       <h1>Blogs</h1>
@@ -9,7 +11,7 @@ export default function BlogsPage() {
       <Link href="/blogs/new">Create new blog</Link>
 
       <ul>
-        {blogs.map((blog) => (
+        {sortedBlogs.map((blog) => (
           <li key={blog.id}>
             <Link href={`/blogs/${blog.id}`}>
               {blog.title}
