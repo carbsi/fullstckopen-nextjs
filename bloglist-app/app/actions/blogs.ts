12c8071d55365b1c433ac805a57074b3c329c5b7
Otto Karppinen <ottokarpp@gmail.com>
2026-08-12 20:55:37 +0300

    Replace in-memory blogs with PostgreSQL

--- bloglist-app/app/actions/blogs.ts
+++ bloglist-app/app/actions/blogs.ts
@@ -1,9 +1,25 @@
 "use server";
 
+import { addBlog, likeBlog } from "../lib/blogs";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
 export async function createBlog(formData: FormData) {
-  // TODO: change to database
+  const title = formData.get("title") as string;
+  const author = formData.get("author") as string;
+  const url = formData.get("url") as string;
+
+  await addBlog(title, author, url);
+
+  revalidatePath("/blogs");
+  redirect("/blogs");
 }
 
 export async function likeBlogAction(formData: FormData) {
-  // TODO: change to database
+  const id = Number(formData.get("id"));
+
+  await likeBlog(id);
+
+  revalidatePath(`/blogs/${id}`);
+  revalidatePath("/blogs");
 }
--- bloglist-app/app/lib/blogs.ts
+++ bloglist-app/app/lib/blogs.ts
@@ -11,3 +11,27 @@ export const getBlogById = async (id: number) => {
     where: eq(blogs.id, id),
   });
 };
+
+export const addBlog = async (
+  title: string,
+  author: string,
+  url: string
+) => {
+  await db.insert(blogs).values({
+    title,
+    author,
+    url,
+    likes: 0,
+  });
+};
+
+export const likeBlog = async (id: number) => {
+  const blog = await getBlogById(id);
+
+  if (blog) {
+    await db
+      .update(blogs)
+      .set({ likes: blog.likes + 1 })
+      .where(eq(blogs.id, id));
+  }
+};
