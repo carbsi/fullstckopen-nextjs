771ff289b068d49c44ab504b37c1645c127642d1
Otto Karppinen <ottokarpp@gmail.com>
2026-08-15 09:36:00 +0300

    exerx_11 now done with /components

---
+++ bloglist-app/app/components/NavBar.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+
+export default function NavBar() {
+  const { data: session } = useSession();
+
+  return (
+    <nav>
+      <Link href="/">home</Link>{" | "}
+      <Link href="/blogs">blogs</Link>{" | "}
+      <Link href="/users">users</Link>{" | "}
+
+      {session ? (
+        <>
+          <em>{session.user?.name} logged in</em>{" "}
+          <button onClick={() => signOut()}>logout</button>
+        </>
+      ) : (
+        <Link href="/login">login</Link>
+      )}
+    </nav>
+  );
+}
