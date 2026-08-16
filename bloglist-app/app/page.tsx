d982a91d064981d3fb7f305ab67f95e1369497bb
Otto Karppinen <ottokarpp@gmail.com>
2026-08-16 21:36:08 +0300

    exercx_22 static homepage with mdx

--- bloglist-app/app/page.tsx
+++ bloglist-app/app/page.tsx
@@ -1,3 +1,9 @@
+import Homepage from "./homepage.mdx"
+
 export default function Home() {
-  return <h1>Blog app</h1>
+  return (
+    <div className="markdown max-w-3xl mx-auto p-6">
+      <Homepage />
+    </div>
+  )
 }
