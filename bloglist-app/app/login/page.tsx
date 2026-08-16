ba79e0d58f57a5ff1c84bb125716704156d9d239
Otto Karppinen <ottokarpp@gmail.com>
2026-08-16 12:18:07 +0300

    login page tyylillistä hieromista

--- bloglist-app/app/login/page.tsx
+++ bloglist-app/app/login/page.tsx
@@ -8,10 +8,11 @@ export default function LoginPage() {
   const router = useRouter()
   const [error, setError] = useState("")
 
-  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget)
+    
 
     // lähettää username ja salasana NextAuth authorize funktiolle auth.ts tiedostossa
     const result = await signIn("credentials", {
@@ -29,27 +30,40 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="max-w-2xl mx-auto p-6">
+      <h2 className="text-xl font-bold mb-4">Login</h2>
 
       {error && <p style={{ color: "red" }}>{error}</p>}
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Username
-            <input type="text" name="username" required />
-          </label>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 max-w-sm">
+        <div className="flex items-center gap-2">
+          <label htmlFor="username" className="w-24">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            className="border border-gray-400 p-1 flex-1"
+          />
         </div>
 
-        <div>
-          <label>
-            Password
-            <input type="password" name="password" required />
-          </label>
+        <div className="flex items-center gap-2">
+          <label htmlFor="password" className="w-24">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            className="border border-gray-400 p-1 flex-1"
+          />
         </div>
 
-        <button type="submit">Login</button>
+        <button
+            type="submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded w-fit"
+          >
+            Login
+        </button>
       </form>
     </div>
   )
